b657141ef18526086062adc2aae5f1f91fba251d
j-crespo <juliancrespo@cca.edu>
2017-09-24 17:55:05 -0700

    Update exercise4.js

--- exercise4.js
+++ exercise4.js
@@ -1,1 +1,22 @@
+background(255);
+noStroke();
+
+x = 0;
+
+while (x < 200) {
+  colorMode(HSB);
+  fill(181, 68, 66);
+  background(255);
+  ellipse(100+x, 100, 30, 30)
+	x = x + 30;
+}
+
+while (x > -80) {
+  colorMode(HSB);
+  fill(181, 68, 66);
+  background(255);
+  ellipse(100+x, 100, 30, 30)
+	x = x - 30;
+}
+
 
